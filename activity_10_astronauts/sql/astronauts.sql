bbc0f55e38f0d7a711c6fa0ac15fb03e0e164b6e
DillonSeacat <111933140+DillonSeacat@users.noreply.github.com>
2023-03-15 20:32:40 -0600

    .
    .

--- activity_10_astronauts/sql/astronauts.sql
+++ activity_10_astronauts/sql/astronauts.sql
@@ -2,3 +2,72 @@
 -- Instructor: Thyago Mota
 -- Description: The astronauts database
 
+CREATE DATABASE astronauts;
+
+DROP TABLE Astronauts;
+
+CREATE TABLE Astronauts (
+    id SERIAL PRIMARY KEY,
+    lastName VARCHAR(35) NOT NULL,
+    firstName VARCHAR(35) NOT NULL,
+    suffix CHAR(10),
+    gender CHAR(1),
+    birth DATE NOT NULL,
+    city VARCHAR(25),
+    state VARCHAR(30),
+    country VARCHAR(25),
+    status VARCHAR(30),
+    daysInSpace INT NOT NULL,
+    flights INT NOT NULL
+);
+
+\copy Astronauts
+    (lastName, firstName, suffix, gender, birth, city, state, country, status, daysInSpace, flights) 
+    from /var/lib/postgresql/data/astronauts.csv DELIMITER ',' CSV HEADER;
+
+
+a) the total number of astronauts. 
+SELECT COUNT(*) FROM Astronauts;
+
+b) the total number of American astronauts.
+SELECT COUNT(*) AS total FROM Astronauts WHERE country = 'USA'; 
+
+c) the list of nationalities of all astronauts in alphabetical order. 
+SELECT DISTINCT country FROM Astronauts ORDER BY 1;
+
+d) all astronaut names ordered by last name (use the format Last Name, First Name, Suffix to display the names). 
+SELECT CONCAT(lastName, ', ', firstName, ', ', suffix) AS name FROM Astronauts ORDER BY 1;
+
+e) the total number of astronauts by gender. 
+SELECT gender, COUNT(*) AS total FROM Astronauts GROUP BY gender;
+
+f) the total number of female astronauts that are still active. 
+
+g) the total number of American female astronauts that are still active. 
+
+h) the list of all American female astronauts that are still active ordered by last name (use the same name format used in d). 
+
+i) the list of Chinese astronauts, displaying only their names and ages (use the same name format used in d). 
+
+j) the total number of astronauts by country. 
+
+SELECT COUNT(*) AS total, country FROM Astronauts 
+
+k) the total number of American astronauts per state ordered by the totals in
+descending order. 
+
+SELECT COUNT(*) AS total, state, FROM, Astronauts WHERE country = 'USA'
+GROUP BY state ORDER BY 1 DESC;
+
+l) the total number of astronauts by statuses (i.e., active or retired). 
+
+
+
+m) name and age of all non-American astronauts in alphabetical order (use the same name format used in d). 
+
+
+
+n) the average age of all American astronauts that are still active. 
+
+
+
--- hwk_05_occupations/src/occupations.sql
+++ hwk_05_occupations/src/occupations.sql
@@ -1,6 +1,6 @@
 -- CS3810: Principles of Database Systems
 -- Instructor: Thyago Mota
--- Student: 
+-- Student: Dillon Seacat
 -- Description: a database of occupations
 
 CREATE DATABASE occupations;
@@ -9,22 +9,30 @@ CREATE DATABASE occupations;
 
 DROP TABLE IF EXISTS Occupations;
 
+
 -- TODO: create table Occupations
-
+CREATE TABLE Occupations( 
+    Code VARCHAR(15) PRIMARY KEY,
+    Occupation VARCHAR(1000) NOT NULL,
+    JobFamily VARCHAR(1000) NOT NULL
+);
 -- TODO: populate table Occupations
-
+\copy Occupations
+    (Code, Occupation, JobFamily)
+    FROM /var/lib/postgresql/data/occupations.csv DELIMITER ',' CSV HEADER;
 -- TODO: a) the total number of occupations (expect 1016).
-
+SELECT COUNT(*) FROM Occupations;
 -- TODO: b) a list of all job families in alphabetical order (expect 23).
-
+SELECT DISTINCT JobFamily FROM Occupations ORDER BY 1;
 -- TODO: c) the total number of job families (expect 23)
-
+SELECT COUNT(DISTINCT JobFamily) FROM Occupations;
 -- TODO: d) the total number of occupations per job family in alphabetical order of job family.
-
+SELECT JobFamily, COUNT(*) FROM Occupations GROUP BY JobFamily ORDER BY 1;
 -- TODO: e) the number of occupations in the "Computer and Mathematical" job family (expect 38)
-
+SELECT COUNT(*) FROM Occupations WHERE JobFamily = 'Computer and Mathematical';
 -- BONUS POINTS
 
 -- TODO: f) an alphabetical list of occupations in the "Computer and Mathematical" job family.
-
+SELECT Occupation FROM Occupations WHERE JobFamily = 'Computer and Mathematical' ORDER BY 1;
 -- TODO: g) an alphabetical list of occupations in the "Computer and Mathematical" job family that begins with the word "Database"
+SELECT Occupation FROM Occupations WHERE JobFamily = 'Computer and Mathematical' AND Occupation LIKE 'Database%' ORDER BY 1;
