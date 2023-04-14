59a1f82c07b20c2fe92123e6d6c9b6e415ea7b41
DillonSeacat <111933140+DillonSeacat@users.noreply.github.com>
2023-04-14 15:06:12 -0600

    .
    .

--- db_02_ipps/src/ipps.py
+++ db_02_ipps/src/ipps.py
@@ -1,20 +1,122 @@
 """
 CS3810: Principles of Database Systems
 Instructor: Thyago Mota
-Student(s): 
+Student(s): Adam Abubakar
 Description: A data load script for the IPPS database
 """
 
 import psycopg2
 import configparser as cp
+import os
+import csv
+
 
 config = cp.RawConfigParser()
 config.read('ConfigFile.properties')
 params = dict(config.items('db'))
-
 conn = psycopg2.connect(**params)
+# check if next line is needed
+conn.autocommit = True
 if conn: 
     print('Connection to Postgres database ' + params['dbname'] + ' was successful!')
 
+    # parse line in file then insert values into the tables
+    file = open("../data/MUP_IHP_RY22_P02_V10_DY20_PrvSvc.csv")
+    data =list(csv.reader(file))
+    # with open(os.path.join("../data","MUP_IHP_RY22_P02_V10_DY20_PrvSvc.csv"), newline= '') as file:
+    #     # csv_reader = csv.reader(file, delimiter=',', quotechar='"')
+    #     # next(csv_reader)
+    #     file.readline() # skip the 1st line
+    for cols in data:
+        # line = line.strip()
+        # i = 0
+        # print(line[1])
+        # cols = line.split(",")
+        xRndrng_Prvdr_CCN = cols[0]
+        xRndrng_Prvdr_Org_Name = cols[1]
+        xRndrng_Prvdr_St = cols[2]
+        xRndrng_Prvdr_City = cols[3]
+        xRndrng_Prvdr_State_Abrvtn = cols[4]
+        xRndrng_Prvdr_State_FIPS = cols[5]
+        xRndrng_Prvdr_Zip5 = cols[6]
+        xRndrng_Prvdr_RUCA = cols[7]
+        print(xRndrng_Prvdr_RUCA)
+        xRndrng_Prvdr_RUCA_Desc = cols[8]
+        xDRG_Cd = cols[9]
+        xDRG_Desc = cols[10]
+        xTot_Dschrgs = cols[11]
+        xAvg_Submtd_Cvrd_Chrg = cols[12]
+        xAvg_Tot_Pymt_Amt = cols[13]
+        xAvg_Mdcr_Pymt_Amt = cols[14]
+        # xRndrng_Prvdr_CCN, xRndrng_Prvdr_Org_Name, xRndrng_Prvdr_St, xRndrng_Prvdr_City, xRndrng_Prvdr_State_Abrvtn, xRndrng_Prvdr_State_FIPS, xRndrng_Prvdr_Zip5, xRndrng_Prvdr_RUCA, xRndrng_Prvdr_RUCA_Desc, xDRG_Cd, xDRG_Desc, xTot_Dschrgs, xAvg_Submtd_Cvrd_Chrg, xAvg_Tot_Pymt_Amt, xAvg_Mdcr_Pymt_Amt= line
+        # print(xRndrng_Prvdr_CCN, xRndrng_Prvdr_Org_Name, xRndrng_Prvdr_St, xRndrng_Prvdr_City, xRndrng_Prvdr_State_Abrvtn, xRndrng_Prvdr_State_FIPS, xRndrng_Prvdr_Zip5, xRndrng_Prvdr_RUCA, xRndrng_Prvdr_RUCA_Desc, xDRG_Cd, xDRG_Desc, xTot_Dschrgs, xAvg_Submtd_Cvrd_Chrg, xAvg_Tot_Pymt_Amt, xAvg_Mdcr_Pymt_Amt)
+        
+        
+        sqlRUCA = '''
+        INSERT INTO RUCA (rndrng_prvdr_ruca, rndrng_prvdr_ruca_desc)
+        VALUES ($1 , $2);
+        '''
+        prepareRUCA = 'prepareRUCA'
+       
+        sqlProvider = '''
+            INSERT INTO Provider (Rndrng_Prvdr_CCN, Rndrng_Prvdr_Org_Name, Rndrng_Prvdr_St, Rndrng_Prvdr_City, Rndrng_Prvdr_State_Abrvtn, Rndrng_Prvdr_State_FIPS, Rndrng_Prvdr_Zip5, ruca_Rndrng_Prvdr_RUCA)
+            VALUES($1, $2, $3, $4 , $5, $6, $7, $8)
+        '''
+        prepareProvider = 'prepareProvider'
+
+        
+        sqlConditions = ''' 
+            INSERT INTO Conditions(DRG_Cd, DRG_Desc) VALUES ($1, $2)
+        '''
+        prepareCondition = 'prepareCondition'
+        
+        sqlBillings = '''
+            INSERT INTO Billing(provider_Rndrng_Prvdr_CCN, conditions_DRG_Cd, Tot_Dschrgs, Avg_Submtd_Cvrd_Chrg, Avg_Tot_Pymt_Amt, Avg_Mdcr_Pymt_Amt)
+            VALUES($1, $2, $3, $4, $5, $6)
+        '''
+        prepareBilling = 'prepareBilling'
+
+        cur = conn.cursor()
+        
+        try:
+            cur.execute('PREPARE {} AS {}'.format(prepareRUCA, sqlRUCA))
+            cur.execute('EXECUTE {} (%s, %s)'.format(prepareRUCA), (xRndrng_Prvdr_RUCA, xRndrng_Prvdr_RUCA_Desc))
+        except Exception as e:
+            print(e)
+            conn.rollback()
+        else:
+            conn.commit()
+
+        try:
+            cur.execute('PREPARE {} AS {}'.format(prepareProvider, sqlProvider))
+            cur.execute('EXECUTE {} (%s, %s, %s, %s, %s, %s, %s, %s)'.format(prepareProvider), 
+                (xRndrng_Prvdr_CCN, xRndrng_Prvdr_Org_Name, xRndrng_Prvdr_St, xRndrng_Prvdr_City, 
+                 xRndrng_Prvdr_State_Abrvtn, xRndrng_Prvdr_State_FIPS, xRndrng_Prvdr_Zip5, xRndrng_Prvdr_RUCA))
+        except Exception as e:
+            print(e)
+            conn.rollback()
+        else:
+            conn.commit()
+
+        try:
+            cur.execute('PREPARE {} AS {}'.format(prepareCondition, sqlConditions))
+            cur.execute('EXECUTE {} (%s, %s)'.format(prepareCondition), (xDRG_Cd, xDRG_Desc))
+            
+        except Exception as e:
+            print(e)
+            conn.rollback()
+        else:
+            conn.commit()
+        try:
+            cur.execute('PREPARE {} AS {}'.format(prepareBilling, sqlBillings))
+            cur.execute('EXECUTE {} (%s, %s, %s, %s, %s, %s)'.format(prepareBilling), (xRndrng_Prvdr_CCN, xDRG_Cd, xTot_Dschrgs, xAvg_Submtd_Cvrd_Chrg, xAvg_Tot_Pymt_Amt, xAvg_Mdcr_Pymt_Amt))
+        except Exception as e:
+            print(e)
+            conn.rollback()
+        else:
+            conn.commit()
+        
+             
+          
     print('Bye!')
     conn.close()
--- db_02_ipps/src/ipps.sql
+++ db_02_ipps/src/ipps.sql
@@ -1,6 +1,6 @@
 -- CS3810: Principles of Database Systems
 -- Instructor: Thyago Mota
--- Student(s): 
+-- Student(s): DILLON SEACAT
 -- Description: IPPS database
 
 DROP DATABASE ipps;
@@ -11,26 +11,82 @@ CREATE DATABASE ipps;
 
 -- create tables
 
+CREATE TABLE RUCA(
+    Rndrng_Prvdr_RUCA DECIMAL PRIMARY KEY,
+    Rndrng_Prvdr_RUCA_Desc VARCHAR(250)
+);
+
+CREATE TABLE Provider (
+    Rndrng_Prvdr_CCN CHAR(5) PRIMARY KEY,
+    Rndrng_Prvdr_Org_Name VARCHAR(100),
+    Rndrng_Prvdr_St VARCHAR(100),
+    Rndrng_Prvdr_City VARCHAR(30),
+    Rndrng_Prvdr_State_Abrvtn CHAR(2),
+    Rndrng_Prvdr_State_FIPS INT, --FOREIGN KEY FOR STATES
+    Rndrng_Prvdr_Zip5 CHAR(5),
+    Rndrng_Prvdr_RUCA DECIMAL
+);
+
+CREATE TABLE Conditions(
+    DRG_Cd CHAR(3) PRIMARY KEY,
+    DRG_Desc VARCHAR(500)
+);
+
+CREATE TABLE Billing(
+    Rndrng_Prvdr_CCN CHAR(5) PRIMARY KEY,
+    DRG_Cd CHAR(3) PRIMARY KEY,
+    Tot_Dschrgs INT,
+    Avg_Submtd_Cvrd_Chrg DECIMAL,
+    Avg_Tot_Pymt_Amt DECIMAL,
+    Avg_Mdcr_Pymt_Amt DECIMAL
+);
+
+
+
 -- create user with appropriate access to the tables
+
+CREATE USER "ipps" PASSWORD '135791';
+GRANT ALL ON TABLE Provider TO "ipps";
+GRANT ALL ON TABLE RUCA TO "ipps";
+GRANT ALL ON TABLE Conditions TO "ipps";
+GRANT ALL ON TABLE Billing TO "ipps";
 
 -- queries
 
 -- a) List all diagnosis in alphabetical order.    
+    SELECT DRG_Desc FROM Conditions ORDER BY DRG_Desc;
 
 -- b) List the names and correspondent states (including Washington D.C.) of all of the providers in alphabetical order (state first, provider name next, no repetition). 
+    SELECT CONCAT(Rndrng_Prvdr_State_Abrvtn, ' ', Rndrng_Prvdr_Org_Name) AS Provider_and_State 
+    FROM Provider GROUP BY Provider_and_State ORDER BY Provider_and_State;
 
 -- c) List the total number of providers.
+    SELECT COUNT(*) AS Total_Providers FROM Provider;
 
 -- d) List the total number of providers per state (including Washington D.C.) in alphabetical order (also printing out the state).  
+    SELECT Rndrng_Prvdr_State_Abrvtn, COUNT(*) AS Total_Providers FROM Provider 
+    GROUP BY Rndrng_Prvdr_State_Abrvtn ORDER BY Rndrng_Prvdr_State_Abrvtn;
 
 -- e) List the providers names in Denver (CO) or in Lakewood (CO) in alphabetical order  
+    SELECT Rndrng_Prvdr_Org_Name FROM Provider 
+    WHERE Rndrng_Prvdr_City IN ('Denver', 'Lakewood') 
+    AND Rndrng_Prvdr_State_Abrvtn = 'CO' 
+    ORDER BY Rndrng_Prvdr_Org_Name;
 
 -- f) List the number of providers per RUCA code (showing the code and description)
+    SELECT r.Rndrng_Prvdr_RUCA, r.Rndrng_Prvdr_RUCA_Desc, COUNT(*) AS Total_Providers 
+    FROM Provider p INNER JOIN RUCA r ON p.Rndrng_Prvdr_RUCA = r.Rndrng_Prvdr_RUCA 
+    GROUP BY r.Rndrng_Prvdr_RUCA, r.Rndrng_Prvdr_RUCA_Desc;
 
 -- g) Show the DRG description for code 308 
+    SELECT DRG_Desc FROM Conditions WHERE DRG_Cd = '308';
 
 -- h) List the top 10 providers (with their correspondent state) that charged (as described in Avg_Submtd_Cvrd_Chrg) the most for the DRG code 308. Output should display the provider name, their city, state, and the average charged amount in descending order.   
+    SELECT CONCAT(Rndrng_Prvdr_Org_Name, ', ', Rndrng_Prvdr_City, ', ', Rndrng_Prvdr_State_Abrvtn) AS Provider, Avg_Submtd_Cvrd_Chrg 
+    FROM Billing b INNER JOIN Provider p ON b.Rndrng_Prvdr_CCN = p.Rndrng_Prvdr_CCN 
+    WHERE DRG_Cd = '308' ORDER BY Avg_Submtd_Cvrd_Chrg DESC LIMIT 10;
 
 -- i) List the average charges (as described in Avg_Submtd_Cvrd_Chrg) of all providers per state for the DRG code 308. Output should display the state and the average charged amount per state in descending order (of the charged amount) using only two decimals. 
+    SELECT 
 
 -- j) Which provider and clinical condition pair had the highest difference between the amount charged (as described in Avg_Submtd_Cvrd_Chrg) and the amount covered by Medicare only (as described in Avg_Mdcr_Pymt_Amt)?
