ebf1e810673f2209a84b6ac7d50ff5fee18f934d
DillonSeacat <111933140+DillonSeacat@users.noreply.github.com>
2023-04-18 10:59:16 -0600

    .
    .

--- db_02_ipps/src/ipps.sql
+++ db_02_ipps/src/ipps.sql
@@ -1,6 +1,6 @@
 -- CS3810: Principles of Database Systems
 -- Instructor: Thyago Mota
--- Student(s): DILLON SEACAT
+-- Student(s): Adam Abubakar and Dillon Seacat
 -- Description: IPPS database
 
 DROP DATABASE ipps;
@@ -17,14 +17,15 @@ CREATE TABLE RUCA(
 );
 
 CREATE TABLE Provider (
-    Rndrng_Prvdr_CCN CHAR(5) PRIMARY KEY,
+    Rndrng_Prvdr_CCN CHAR(6) PRIMARY KEY,
     Rndrng_Prvdr_Org_Name VARCHAR(100),
     Rndrng_Prvdr_St VARCHAR(100),
     Rndrng_Prvdr_City VARCHAR(30),
     Rndrng_Prvdr_State_Abrvtn CHAR(2),
-    Rndrng_Prvdr_State_FIPS INT, --FOREIGN KEY FOR STATES
+    Rndrng_Prvdr_State_FIPS INT,
     Rndrng_Prvdr_Zip5 CHAR(5),
-    Rndrng_Prvdr_RUCA DECIMAL
+    ruca_Rndrng_Prvdr_RUCA DECIMAL,
+    FOREIGN KEY(ruca_Rndrng_Prvdr_RUCA) REFERENCES RUCA(Rndrng_Prvdr_RUCA)
 );
 
 CREATE TABLE Conditions(
@@ -33,23 +34,25 @@ CREATE TABLE Conditions(
 );
 
 CREATE TABLE Billing(
-    Rndrng_Prvdr_CCN CHAR(5) PRIMARY KEY,
-    DRG_Cd CHAR(3) PRIMARY KEY,
+    provider_Rndrng_Prvdr_CCN CHAR(6),
+    conditions_DRG_Cd CHAR(3),
     Tot_Dschrgs INT,
     Avg_Submtd_Cvrd_Chrg DECIMAL,
     Avg_Tot_Pymt_Amt DECIMAL,
-    Avg_Mdcr_Pymt_Amt DECIMAL
+    Avg_Mdcr_Pymt_Amt DECIMAL,
+    PRIMARY KEY(provider_Rndrng_Prvdr_CCN, conditions_DRG_Cd),
+    FOREIGN KEY(provider_Rndrng_Prvdr_CCN) REFERENCES Provider(Rndrng_Prvdr_CCN),
+    FOREIGN KEY(conditions_DRG_Cd) REFERENCES Conditions(DRG_Cd)
 );
 
 
-
 -- create user with appropriate access to the tables
-
 CREATE USER "ipps" PASSWORD '135791';
 GRANT ALL ON TABLE Provider TO "ipps";
 GRANT ALL ON TABLE RUCA TO "ipps";
 GRANT ALL ON TABLE Conditions TO "ipps";
 GRANT ALL ON TABLE Billing TO "ipps";
+
 
 -- queries
 
@@ -57,8 +60,13 @@ GRANT ALL ON TABLE Billing TO "ipps";
     SELECT DRG_Desc FROM Conditions ORDER BY DRG_Desc;
 
 -- b) List the names and correspondent states (including Washington D.C.) of all of the providers in alphabetical order (state first, provider name next, no repetition). 
+    -- ********************
     SELECT CONCAT(Rndrng_Prvdr_State_Abrvtn, ' ', Rndrng_Prvdr_Org_Name) AS Provider_and_State 
-    FROM Provider GROUP BY Provider_and_State ORDER BY Provider_and_State;
+    FROM Provider ORDER BY Provider_and_State;
+
+    SELECT Rndrng_Prvdr_State_Abrvtn, Rndrng_Prvdr_Org_Name 
+    FROM Provider; GROUP BY Provider_and_State ORDER BY Provider_and_State;
+    -- ********************
 
 -- c) List the total number of providers.
     SELECT COUNT(*) AS Total_Providers FROM Provider;
@@ -74,19 +82,24 @@ GRANT ALL ON TABLE Billing TO "ipps";
     ORDER BY Rndrng_Prvdr_Org_Name;
 
 -- f) List the number of providers per RUCA code (showing the code and description)
-    SELECT r.Rndrng_Prvdr_RUCA, r.Rndrng_Prvdr_RUCA_Desc, COUNT(*) AS Total_Providers 
-    FROM Provider p INNER JOIN RUCA r ON p.Rndrng_Prvdr_RUCA = r.Rndrng_Prvdr_RUCA 
-    GROUP BY r.Rndrng_Prvdr_RUCA, r.Rndrng_Prvdr_RUCA_Desc;
+    SELECT RUCA.Rndrng_Prvdr_RUCA, RUCA.Rndrng_Prvdr_RUCA_Desc, COUNT(*) AS Total_Providers 
+    FROM Provider INNER JOIN RUCA ON Provider.ruca_Rndrng_Prvdr_RUCA = RUCA.Rndrng_Prvdr_RUCA 
+    GROUP BY RUCA.Rndrng_Prvdr_RUCA, RUCA.Rndrng_Prvdr_RUCA_Desc;
 
 -- g) Show the DRG description for code 308 
     SELECT DRG_Desc FROM Conditions WHERE DRG_Cd = '308';
 
 -- h) List the top 10 providers (with their correspondent state) that charged (as described in Avg_Submtd_Cvrd_Chrg) the most for the DRG code 308. Output should display the provider name, their city, state, and the average charged amount in descending order.   
-    SELECT CONCAT(Rndrng_Prvdr_Org_Name, ', ', Rndrng_Prvdr_City, ', ', Rndrng_Prvdr_State_Abrvtn) AS Provider, Avg_Submtd_Cvrd_Chrg 
-    FROM Billing b INNER JOIN Provider p ON b.Rndrng_Prvdr_CCN = p.Rndrng_Prvdr_CCN 
-    WHERE DRG_Cd = '308' ORDER BY Avg_Submtd_Cvrd_Chrg DESC LIMIT 10;
+    SELECT CONCAT(p.Rndrng_Prvdr_Org_Name, ', ', p.Rndrng_Prvdr_City, ', ', p.Rndrng_Prvdr_State_Abrvtn) AS Provider, b.Avg_Submtd_Cvrd_Chrg 
+    FROM Billing b INNER JOIN Provider p ON b.provider_Rndrng_Prvdr_CCN = p.Rndrng_Prvdr_CCN 
+    WHERE b.conditions_DRG_Cd = '308' ORDER BY b.Avg_Submtd_Cvrd_Chrg DESC LIMIT 10;
 
 -- i) List the average charges (as described in Avg_Submtd_Cvrd_Chrg) of all providers per state for the DRG code 308. Output should display the state and the average charged amount per state in descending order (of the charged amount) using only two decimals. 
-    SELECT 
+    SELECT p.Rndrng_Prvdr_State_Abrvtn AS state , ROUND(AVG(b.Avg_Submtd_Cvrd_Chrg),2) AS average_amount_charged
+    FROM Billing b INNER JOIN Provider p ON b.provider_Rndrng_Prvdr_CCN = p.Rndrng_Prvdr_CCN 
+    WHERE b.conditions_DRG_Cd = '308' GROUP BY state ORDER BY average_amount_charged DESC;
 
 -- j) Which provider and clinical condition pair had the highest difference between the amount charged (as described in Avg_Submtd_Cvrd_Chrg) and the amount covered by Medicare only (as described in Avg_Mdcr_Pymt_Amt)?
+    SELECT A.Rndrng_Prvdr_Org_Name, C.DRG_Desc, ROUND((B.Avg_Submtd_Cvrd_Chrg - B.Avg_Mdcr_Pymt_Amt),2) AS difference_between_amount_charged_and_medicare 
+    FROM Provider A INNER JOIN Billing B ON A.Rndrng_Prvdr_CCN = B.provider_Rndrng_Prvdr_CCN INNER JOIN Conditions C ON B.conditions_DRG_Cd = C.DRG_Cd 
+    ORDER BY difference_between_amount_charged_and_medicare DESC LIMIT 1;
--- hwk_08_audit/sql/audit.sql
+++ hwk_08_audit/sql/audit.sql
@@ -1,6 +1,6 @@
 -- CS3810: Principles of Database Systems
 -- Instructor: Thyago Mota
--- Student: 
+-- Student: DILLON SEACAT
 -- Description: SQL for the audit database
 
 DROP DATABASE audit;
@@ -21,9 +21,20 @@ CREATE TABLE EmployeesAudit (
 );
 
 --- CREATE FUNCTION employee_audit_after_insert() RETURNS TRIGGER
-
+CREATE FUNCTION employee_audit_after_insert() RETURNS TRIGGER
+    LANGUAGE plpgsql
+    AS $$
+        BEGIN
+            INSERT INTO EmployeesAudit (date, descr)
+            VALUES (CURRENT_DATE, NEW.id || ',' || NEW.name);
+            RETURN NEW;
+        END;
+    $$;
 -- CREATE TRIGGER employee_audit
-
+CREATE TRIGGER employee_audit
+    AFTER INSERT ON Employees
+    FOR EACH ROW
+    EXECUTE PROCEDURE employee_audit_after_insert();
 -- use the following insert statements to test your trigger
 INSERT INTO Employees VALUES (101, 'Samuel Adams'); 
 INSERT INTO Employees VALUES (202, 'Adolph Coors');
