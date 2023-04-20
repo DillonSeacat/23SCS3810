fb109289c7078ba2325b3e68dbeb785f6bab0baa
DillonSeacat <111933140+DillonSeacat@users.noreply.github.com>
2023-04-20 11:03:32 -0600

    .
    .

--- hwl_09_orm/src/campers.py
+++ hwl_09_orm/src/campers.py
@@ -19,11 +19,11 @@ This class represents a camper entity
 class Camper(Base): 
     # TODO #1 complete the mapping of class Camper and table campers
     '''
-    __tablename__ = ...
-    id     = ...
-    name   = ...
-    dob    = ...
-    gender = ...
+    __tablename__ = "campers"
+    id     = Column(Integer, primary_key=True)
+    name   = Column(String)
+    dob    = Column(Date)
+    gender = Column(String)
     '''
     
     # programs will allow the retrieval of all programs that a camper is in via the intermediate table "participates"
@@ -41,9 +41,9 @@ This class represents the intermediate table between campers and programs
 class Participate(Base):
     # TODO #2 complete the mapping of class Participate and table participates
     '''
-    __tablename__ = ...
-    program = Column(..., ForeignKey(...), primary_key=True) 
-    camper = Column(..., ForeignKey(...), primary_key=True)
+    __tablename__ = "participates"
+    program = Column(String, ForeignKey("programs.name"), primary_key=True) 
+    camper = Column(Integer, ForeignKey("campers.id"), primary_key=True)
     '''
 
 '''
@@ -52,10 +52,10 @@ This class represents a program entity
 class Program(Base):
     # TODO #3 complete the mapping of class Program and table programs
     '''
-    __tablename__ = ...
-    name = Column(..., primary_key=True) 
-    descr = Column(...)
-    price = Column(...)
+    __tablename__ = "programs"
+    name = Column(String, primary_key=True) 
+    descr = Column(String)
+    price = Column(Integer)
     '''
 
     # campers will allow the retrieval of all campers that are enrolled in a program via the intermediate table "participates"
@@ -84,11 +84,17 @@ if engine:
     session = Session()
     # TODO #4 ask the user for the name of a program 
     '''
-    name = ... 
+    name = input("please enter the name of a program: ")
     '''
 
     # retrieves program info without any SQL (wow!)
     program = session.get(Program, name)
 
     # TODO #5 check if the program exists and if yes show the program info including all campers enrolled in the program 
-    
+    if program:
+        print("Program: ", program)
+        print("Campers enrolled: ")
+        for camper in program.campers:
+            print(camper)
+    else:
+        print("Program not found.")
